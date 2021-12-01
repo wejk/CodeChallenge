f309430b851dc4f0877884f8e70f56df17192e22
ken <wejk@outlook.com>
2021-12-01 20:39:29 +1000

    Add home controller

---
+++ WebApi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public string Home()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd");
+        }
+
+    }
+}
